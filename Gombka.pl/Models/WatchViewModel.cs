801376b71f47ade34a9c73615a08cdb7e6168d94
Jarosław Żak <shado.ackerly@gmail.com>
2021-02-08 22:30:00 +0100

    watch page

---
+++ Gombka.pl/Models/WatchViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gombka.pl.Models.Entities;
+
+namespace Gombka.pl.Models
+{
+    public class WatchViewModel
+    {
+        public VideoEntity Video { get; set; }
+        public VoteEntity? VoteEntity { get; set; }
+    }
+}
