29baba02d2bf292733af49551a1a90c977fd3c24
zer0xday <majcher10@gmail.com>
2021-10-31 16:50:17 +0100

    Feat: help tab with mock data

---
+++ Gombka.pl/Controllers/HelpController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gombka.pl.Controllers
+{
+    public class HelpController : Controller
+    {
+        [Route("/pomoc")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
